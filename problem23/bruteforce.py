6f8898814a852d30684754693ed7fad666e5e58b
Grant Cooksey <grantcooksey@users.noreply.github.com>
2017-05-08 09:19:34 -0400

    Used a set with the new optimizations.

--- problem23/bruteforce.py
+++ problem23/bruteforce.py
@@ -29,22 +29,17 @@ def count_non_abundant():
         if sum_divisors(i) > i:
             abundant_numbers.append(i)
 
-
-    #d = {x + y for x in abundant_numbers for y in abundant_numbers}
-
-    deficient_list = [1] * end
-    for i in range(len(abundant_numbers)):
-        for j in range(len(abundant_numbers)):
-            if i < j:
+    abundant_sums = set()
+    for x in abundant_numbers:
+        for y in abundant_numbers:
+            if x < y or x + y >= end:
                 break
-            if abundant_numbers[j] + abundant_numbers[i] < end:
-                deficient_list[abundant_numbers[j] + abundant_numbers[i]] = 0
+            else:
+                abundant_sums.add(x + y)
 
     non_abundant_sum = 0
     for i in range(1, end):
-        #if i not in d:
-        #    non_abundant_sum += i
-        if deficient_list[i] == 1:
+        if i not in abundant_sums:
             non_abundant_sum += i
 
     return non_abundant_sum
