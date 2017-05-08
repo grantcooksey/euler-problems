becc90a315ac9beb9fff7c1fa7550248a55d6f26
Grant Cooksey <grantcooksey@users.noreply.github.com>
2017-05-08 09:30:17 -0400

    Initial commit

---
+++ problem24/bruteforce.py
@@ -0,0 +1,13 @@
+import time
+
+
+def main():
+    start = time.time()
+    ans = 1  #TODO start problem here
+    spent = time.time() - start
+
+    print('{0} found in {1} seconds'.format(ans, spent))
+
+
+if __name__ == '__main__':
+    main()
