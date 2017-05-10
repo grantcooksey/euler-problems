b79fed050a05d697a1950397cc2b0064cdd33e1b
cooksey <suitcase24@gmail.com>
2017-05-10 13:05:30 -0400

    Added test file.

---
+++ problem26/test_calc_cycle.py
@@ -0,0 +1,11 @@
+import unittest
+from bruteforce import calc_cycle
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
