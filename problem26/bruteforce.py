1336c0281c2090965fda7e3249b7d0deb95e5c73
cooksey <suitcase24@gmail.com>
2017-05-10 13:06:07 -0400

    solution skeleton.

--- problem26/bruteforce.py
+++ problem26/bruteforce.py
@@ -1,14 +1,39 @@
 import time
 import common
+from math import floor
+
+
+def calc_cycle(d):
+    n = r = 1
+    rl = list()
+    while r != 0:
+        n *= 10
+        r = n - (d * floor(n / d))
+        if r in rl:
+            break
+        rl.append(r)
+    return len(rl) - rl.index(r)
+
+
+def cycles():
+    best_cycle = 0
+    best_num = 0
+    for x in xrange(1000, 1, -1):
+        if x < best_cycle:
+            break
+        c = calc_cycle(x)
+        if c > best_cycle:
+            best_cycle = c
+            best_num = x
+    return best_num
 
 
 def main():
     start = time.time()
-    ans = 1/float(1000)  #TODO start problem here
+    ans = cycles()
     spent = time.time() - start
 
     print('{0} found in {1} seconds'.format(ans, spent))
-    common.hello()
 
 
 if __name__ == '__main__':
