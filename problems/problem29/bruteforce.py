bbbf01a324fae83aafe57fe43f803c919f9e9932
cooksey <suitcase24@gmail.com>
2017-05-12 16:32:16 -0400

    Problem skeleton added.

---
+++ problems/problem29/bruteforce.py
@@ -0,0 +1,14 @@
+import time
+import common
+
+
+def main():
+    start = time.time()
+    ans = 1  #TODO start problem here
+    spent = time.time() - start
+
+    print('{0} found in {1} seconds'.format(ans, spent))
+
+
+if __name__ == '__main__':
+    main()
