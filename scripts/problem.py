6537461a7533cddf8ead22561f5f334edf107afb
cooksey <suitcase24@gmail.com>
2017-06-05 11:44:15 -0400

    Make request and parse.

--- scripts/problem.py
+++ scripts/problem.py
@@ -1,9 +1,13 @@
 import sys
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
+from lxml import html
+
+
+def split_into_lines(s):
+    return s
 
 
 def main(argv):
-
     # Check that all arguments are integers
     try:
         problems = [int(item) for item in argv]
@@ -12,14 +16,29 @@ def main(argv):
         sys.exit(1)
 
     for problem_number in problems:
-        pass
-        # TODO: Make request to euler
-        # TODO: Parse title and capitalize first letters
-        # TODO: Parse out question info
-        # TODO: Split string into lines less than 80 char long
+        url = 'https://projecteuler.net/problem=' + str(problem_number)
+
+        # Make request to euler
+        try:
+            page = urlopen(url, timeout=5)
+            content = html.fromstring(page.read())
+        except URLError:
+            print('Error: The request to {0} did not complete.'.format(url))
+            sys.exit(1)
+
+        # Parse title and capitalize first letters
+        title = content.xpath('//h2').text.title()
+
+        # Parse out question info
+        el = content.xpath('//div[@class="problem_content"]/child::*')
+        description_long = '\n'.join(x.text for x in el)
+
+        # Split string into lines less than 80 char long
+        description = split_into_lines(description_long)
+
         # TODO: Create problem directory
         # TODO: Copy README and code skeleton over
-        # TODO: R eplace README title with problem title
+        # TODO: Replace README title with problem title
         # TODO: Replace README description with problem description
         # TODO: Enhancement. Generate method in code template
 
