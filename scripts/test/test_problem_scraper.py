d108153e461f2015a12dc164830f8009f67e4b3f
cooksey <suitcase24@gmail.com>
2017-06-05 16:02:26 -0400

    Fixed bug in test case.

--- scripts/test/test_problem_scraper.py
+++ scripts/test/test_problem_scraper.py
@@ -17,18 +17,18 @@ class TestSplitIntoLines(TestCase):
         self.assertEqual(parsed_test, text)
 
     def test_split_into_lines_37_long(self):
-        text = 'The number \n' + \
-            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' + \
-            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n' + \
-            '3797 has an interesting property. Being prime ' + \
-            'itself, it is possible \nto continuously remove digits ' + \
-            'from left to right, and remain prime at each \nstage: 3797, ' + \
-            '797, 97, and 7. Similarly we can work from right to left: ' + \
-            '3797, \n379, 37, and 3. \n\nFind the sum of the only eleven ' + \
-            'primes that are both truncatable from left to \nright and ' + \
-            'right to left. \n\nNOTE: 2, 3, 5, and 7 are not considered ' + \
-            'to be truncatable primes. \n\n'
+        text = 'The number aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' \
+               'aaaaaaaaaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 3797' \
+               ' has an interesting property. Being prime \nitself, it is ' \
+               'possible to continuously remove digits from left to right,' \
+               ' and \nremain prime at each stage: 3797, 797, 97, and 7. S' \
+               'imilarly we can work from \nright to left: 3797, 379, 37, ' \
+               'and 3. \n\nFind the sum of the only eleven primes that are ' \
+               'both truncatable from left to \nright and right to left. \n' \
+               '\nNOTE: 2, 3, 5, and 7 are not considered to be truncatable' \
+               ' primes. \n\n'
         x, description = parse_problem(make_request(37))
         long_word = ' ' + ''.join('a' for a in range(100))
         description = description[:10] + long_word + description[10:]
+        print(description)
         self.assertEqual(split_into_lines(description), text)
