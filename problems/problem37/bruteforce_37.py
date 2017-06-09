dba8348b23104d17817ffd034daebd7bbd4babe7
cooksey <suitcase24@gmail.com>
2017-06-09 17:06:08 -0400

    Added filtering to prime list.

--- problems/problem37/bruteforce_37.py
+++ problems/problem37/bruteforce_37.py
@@ -17,22 +17,32 @@ def truncate(prime, prime_set):
     curr = prime
     while len(left) > 1:
         first_digit = left.pop(-1)
-        curr -= first_digit * 10**len(left)
+        curr -= first_digit * 10 ** len(left)
         if curr not in prime_set:
             return False
 
     return True
 
 
+def first_last_digit_prime(x, primes):
+    last_digit = x % 10
+    first_digit = x / 10**(common.get_num_digits(x) - 1)
+    return last_digit in primes and first_digit in primes
+
+
 def truncatable_primes():
     primes = common.prime_sieve(1000000)
     prime_set = set(primes)
 
-    # Todo filter
+    under_10 = primes[:4]
+    primes = primes[4:]  # Ignore single digits primes
+
+    # Filter primes where first and last digit are not prime
+    # primes = filter(lambda x: first_last_digit_prime(x, under_10), primes)
 
     truncatable_sum = 0
 
-    for p in primes[4:]:  # Ignore single digits primes
+    for p in primes:
         if truncate(p, prime_set):
             truncatable_sum += p
 
