23daa6e2d1f503dcdd355bd8134d84ab8bc605f0
cooksey <suitcase24@gmail.com>
2017-06-13 11:42:33 -0400

    Cleaned up solution.

--- problems/problem38/bruteforce_38.py
+++ problems/problem38/bruteforce_38.py
@@ -2,43 +2,35 @@ import time
 import common
 
 
-def is_pandigital(x, n):
+def is_pandigital(x):
     val = str()
     val_set = set()
 
     # Build potential pandigital
-    for i in xrange(1, n + 1):
+    i = 1
+    while len(val) < 9:
         val += str(x * i)
+        i += 1
 
-    # Elimitate non unique characters in potential pandigital
+    # Eliminate non unique characters in potential pandigital
     for c in val:
         val_set.add(c)
 
     # Check for 1-9 pandigital
-    if len(val) == 9 and len(val_set) == 9 and '0' not in val_set:
+    if len(val) == len(val_set) and '0' not in val_set:
         return True, val
     else:
         return False, val
 
 
 def pandigital_multiples():
-    x = 10**4 - 1
-    r = 3
-    n = 2
-    end = 10**r
     pandigital_list = list()
-    while r > 1:
-        pan, val = is_pandigital(x, n)
+    for x in range(2, 10000):
+        pan, val = is_pandigital(x)
         if pan:
             pandigital_list.append(val)
-        x -= 1
-        if x <= end:
-            r -= 1
-            end = 10**r
-            n += 1
 
-    pandigital_list.sort()
-    return pandigital_list[-1]
+    return max(pandigital_list)
 
 
 def main():
